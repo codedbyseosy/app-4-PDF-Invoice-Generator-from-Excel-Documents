048b8c4b2c61adc7e9f51a0f3ef9e3859589235e
eseoseodion <ehodion1@sheffield.ac.uk>
2023-10-29 20:26:16 +0000

    Added an invoive date

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
-#glob is used to return all file paths that match a specific pattern
+# glob is used to return all file paths that match a specific pattern
 filepaths = glob.glob("/Users/eseoseodion/Documents/Python 2023/Visual Code/UDEMY_PROJECTS/app-4/Invoices/*.xlsx") #get every file that ends in .xlsx
 
 for filepath in filepaths:
@@ -14,10 +14,21 @@ for filepath in filepaths:
     pdf = FPDF(orientation="P", unit="mm", format="A4") #1 pdf per invoice
     pdf.add_page()
 
-    filename = Path(filepath).stem #toextract a part of the file path
+    # Extract the name of the file w/o the date
+    filename = Path(filepath).stem #to extract a part of the file path
     #print(filename)
     invoice_nr = filename.split("-")[0]
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"invoice nr.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"invoice nr.{invoice_nr}", ln=1) # ln creates a breakline
+    
+
+    # Extract the name of the file that includes the date
+    date = filename.split("-")[1]
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date {date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
+    
+   
